fe251b85f4890d5058dec8ca0ae499bd771c0170
Jonas Franz <info@jonasfranz.software>
2016-04-13 15:09:25 +0200

    Added basic of Plugin-API

---
+++ src/main/java/net/cleverdesk/cleverdesk/listener/ListenerManager.kt
@@ -0,0 +1,25 @@
+/*
+ * Cleverdesk, a modular helpdesk
+ *
+ *   Copyright (C) 2016 Jonas Kuche
+ *   Copyright (C) 2016 Jonas Franz
+ *   Copyright (C) 2016 Konrad Langenberg
+ *
+ *   This file is part of Cleverdesk.
+ *
+ *   Cleverdesk is free software: you can redistribute it and/or modify it under the terms of the
+ *   GNU General Public License as published by the Free Software Foundation, either version 2 of the
+ *   License, or (at your option) any later version.
+ *
+ *   Cleverdesk is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *   See the GNU General Public License for more details. You should have received a copy of the GNU
+ *   General Public License along with Cleverdesk. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package net.cleverdesk.cleverdesk.listener
+
+interface ListenerManager : List<Listener> {
+}
