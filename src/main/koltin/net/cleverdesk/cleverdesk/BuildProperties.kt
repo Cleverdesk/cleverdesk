473ac7723523bf3bca9a11991ce8b62140fea4a6
JonasFranzDEV <info@jonasfranz.software>
2016-06-21 17:03:23 +0200

    * Fixed kotlin/-Bug

--- src/main/koltin/net/cleverdesk/cleverdesk/BuildProperties.kt
+++
@@ -1,9 +0,0 @@
-package net.cleverdesk.cleverdesk
-
-/**
- * Created by schulerlabor on 21.06.16.
- */
-object BuildProperties {
-    val VERSION: String = "1.1.0"
-    val TYPE: String = "ALPHA-DEVELOPMENT"
-}
